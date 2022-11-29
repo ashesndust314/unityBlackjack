12c58e6f5e2b41e7e12796b3fc9d045749ea4954
ashesndust314 <ashleymkang@gmail.com>
2022-11-29 11:18:57 -0700

    added person and card class

---
+++ unityBlackjack/Card.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    /// <summary>
+    /// Svojstvo koje odredjuje kartu
+    /// </summary>
+    public string ID { get; set; }
+
+    /// <summary>
+    /// Svojstvo koje nam kaze kolika je blackjack vrijednost karte
+    /// </summary>
+    public int Card_Value { get; set; }
+    /// <summary>
+    /// Svojstvo koje govori je li karta broj ili znak sluzi za dodavanje karata na ekran
+    /// </summary>
+    public bool IDNumber
+    {
+        get
+        {
+            if (ID == "J" || ID == "Q" || ID == "K" || ID == "A") return true;
+            else return false;
+        }
+    }
+
+    //konstruktori
+    public Card()
+    { ID = ""; Card_Value = 0; }
+    public Card(string ID_karte)//prima string ID,dakle oznaku karte,brojeve standardno te "J","Q","K","A"
+    {
+        ID = ID_karte;
+        //ako se moze pretvoriti u broj
+        try
+        {
+            Card_Value = int.Parse(ID_karte);//karte 2,3,4,5,6,7,8,9,10
+        }
+        catch //ako se ne moze pretvoriti u broj
+        {
+            if (ID_karte == "J" || ID_karte == "Q" || ID_karte == "K")//ako je karta J,Q ili K,vrijednost je 10
+            {
+                Card_Value = 10;
+            }
+            else Card_Value = 11;//ako je karta as,defaultno se stavi na pocetak 11,kasnije se mjenja tijekom igre po potrebi
+        }
+    }
+}
---
+++ unityBlackjack/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public Person()
+    {
+        LastCard_X = 0;
+        LastCard_Y = 0;
+        Active = false;
+        SoftHand = false;
+        r = new Random();
+    }
+    
+    //List of cards a person has in their hand
+    public List<Card> Hand { get; set; }
+
+    private Random r; //random object
+
+    //Sets the coordinates for the next card that will appear on the screen
+    public void SetNewCard(int x, int y)
+    {
+        LastCard_X = x; LastCard_Y = y;
+    }
+
+    //Numerical value of the cards the person has in their hand
+    public int HandValue
+    {
+        get
+        {
+            int aceCount = 0; //number of aces a person has in their hand
+            int handValue = 0; //total value of the cards
+            for (int i = 0; i < Hand.Count; i++)
+            {
+                if (Hand[i].ID == "A")
+                {
+                    aceCount += 1;
+                }
+                else //cards are aren't aces
+                {
+                    handValue += Hand[i].Card_Value;
+                }
+            }
+            SoftHand = false; //soft hand is false by default
+
+            //check if any aces have a value of 11
+            if (aceCount > 0) //if you have multiple aces, only one can have a value of 11
+            {
+                if ((handValue + 11 + aceCount - 1) <= 21)
+                {
+                    handValue = handValue + 11 + aceCount - 1; //set one ace as value of 11, other aces with value of 1
+                    SoftHand = true; //setting soft hand to true
+                }
+                else //if no ace can have a value of 11 (otherwise bust), all aces have value of 1
+                {
+                    handValue += aceCount;
+                }
+            }
+            return handValue;
+        }
+    }
+
+   //The property of cards that a player has, if an ace has a value of 11, then SoftHand is true, otherwise it is false
+    public bool SoftHand { get; set; }
+
+    //check the position of the last card of the player on the screen, if the position is (0,0), then there are no cards
+    public int LastCard_X { get; set; }
+
+    public int LastCard_Y { get; set; }
+
+    //to know if the player is active, aka if they are currently playing
+    public bool Active { get; set; }
+
+    //Adding a new card for the player
+    public void AddNewCard(Card newCard)
+    {
+        //string image path
+        string putanja = "sprites\\cards2\\" + newCard.ID.ToString() + "\\" + r.Next(1, 5).ToString() + ".png";
+        Hand.Add(newCard); //adding a new card to the list of cards a player has
+        Sprite card = new Sprite(putanja, LastCard_X, LastCard_Y, BlackjackGame.Sirina, BlackjackGame.Visina, "card"); //making a new sprite
+        Game.AddSprite(card); //Add a card to the game
+        SetNewCard(LastCard_X + 15, LastCard_Y); //setting the position of the new card
+    }
+}
